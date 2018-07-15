bf45a4ac5385a41c04f255bf77a805695fd676cb
Qin Hao <qh@mail.sdu.edu.cn>
2018-07-15 10:57:51 +0800

    updated component lifecycle

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,18 +1,91 @@
 import React from 'react'
 
 export class Home extends React.Component{
+	constructor(props){
+			super()
+			this.state = {
+				age : props.initialAge,
+				homeLink : props.initialLinkName,
+				status : 0
+			}
+			console.log("constructor")
+			setTimeout(()=>{
+				// this.state.status += 1
+				this.setState({
+					status : 1
+				})
+			}, 3000)
+		}
+
+	
+
+	onGrowOlder(){
+		this.setState({
+			age : this.state.age + 3
+		})
+	}
+	onChangeLink(){
+		this.props.changeLink(this.state.homeLink)
+	}
+	onChangeInput(event){
+		this.setState({
+			homeLink : event.target.value
+		})
+	}
+
+	// Component lifecycle
+
+	componentWillMount(){
+		console.log("Component will mount")
+	}
+
+	componentDidMount(){
+		console.log("Component did mount")
+	}
+
+	componentWillReceiveProps(nextProps){
+		console.log("Component will receive props", nextProps)
+	}
+
+	shouldComponentUpdate(nextProps, nextState){
+		console.log("Should Component update", nextProps, nextState)
+		return true
+	}
+
+	componentWillUpdate(nextProps, nextState){
+		console.log("Component will update", nextProps, nextState)
+	}
+
+	componentDidUpdate(prevProps, prevState){
+		console.log("Component did update", prevProps, prevState)
+	}
+
+	componentWillUnmount(){
+		console.log("Component will Unmount")
+	}
+
+	//
+
 	render(){
 		return(
 			<div>
 				<p>I'm a new Component!</p>
 				{ 5 < 2 ? 'Yes' : 'No' }
-				<p>the name is {this.props.name} and the age is {this.props.age}</p>
+
+				<p>the name is {this.props.name} and the age is {this.state.age}</p>
 				<p>ObjectName => {this.props.user.name}</p>
-				<p>{this.props.user.hobbies.map(hobby => <li>{hobby}</li>)}</p>
-
-
+				<p>Status: {this.state.status}</p>
+				<button type="button" className="btn btn-primary" onClick={() => this.onGrowOlder()}>
+					Make me older
+				</button>
+				<hr/>
+				<input type="text" value={this.state.homeLink} onChange={(event)=>this.onChangeInput(event)}/>
+				<button type="button" className="btn btn-primary" onClick={()=>this.onChangeLink()}>
+					Change
+				</button>
 			</div>
 		);
 	}
 }
 
+//() => this.onGrowOlder() or this.onGrowOlder().bind(this)
--- src/index.js
+++ src/index.js
@@ -5,21 +5,62 @@ import {Header} from './components/Header'
 import {Home} from './components/Home'
 
 class App extends React.Component{
+	constructor(){
+		super()
+		this.state = {
+			homeLink : "Home",
+			homeMounted : true
+		}
+	}
+
+	onChangeLinkFunc(newLink){
+		// this.state.homeLink = newLink
+		this.setState({
+			homeLink : newLink
+		})
+	}
+
+	onChangeHomeMounted(){
+		this.setState({
+			homeMounted : ! this.state.homeMounted
+		})
+	}
+
 	render(){
 		var user = {
 			name : "Max",
 			hobbies : ["Football","Swimming"],
 		}
+
+		let homeCmp = ""
+		if (this.state.homeMounted){
+			homeCmp = (
+				<Home name="Howard" 
+    				  initialAge={19} 
+    				  user={user} 
+    				  changeLink={(newLink)=>this.onChangeLinkFunc(newLink)}
+    				  initialLinkName={this.state.homeLink} >
+            	</Home>
+			)
+		}
+
 		return(
 			<div className="container">
             	<div className="row">
             		<div className="col-xs-10 col-xs-offset-1">
-            			<Header/>
+            			<Header homeLink={this.state.homeLink}></Header>
             		</div>
             	</div>
             	<div className="row">
             		<div className="col-xs-10 col-xs-offset-1">
-            			<Home name="Howard" age={19} user={user}> </Home>
+            			{homeCmp}
+            		</div>
+            	</div>
+            	<div className="row">
+            		<div className="col-xs-10 col-xs-offset-1">
+            			<button type="button" className="btn btn-primary" onClick={()=>this.onChangeHomeMounted()}>
+            				Show Home Component
+            			</button>
             		</div>
             	</div>
             </div>
@@ -27,5 +68,5 @@ class App extends React.Component{
 	}
 }
 
-
+//changeLink={this.onChangeLinkFunc().bind(this)} ----- cannot use es6 arrow func
 render(<App/>, window.document.getElementById('app'));
